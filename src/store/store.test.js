524af0b95b60c9ade282380f7b03edbbdf3d0718
Vladimir Ratsev <vratsev@gmail.com>
2017-06-21 12:13:24 +0500

    Achieve 100% coverage for tests

--- src/store/store.test.js
+++ src/store/store.test.js
@@ -1,5 +1,10 @@
 import configureStore from './store'
 
+jest.mock('redux-logger')
+const logger = require('redux-logger')
+
+logger.default = jest.fn(() => next => action => next(action))
+
 test('should create store', () => {
   const store = configureStore()
   expect(store).not.toBeUndefined()
@@ -11,10 +16,21 @@ test('should inject reducer and patch store', () => {
   }
   const reducer = state => state
   const store = configureStore(null, {
-    test: data,
+    test1: data,
+    test2: null,
   })
   expect(store.getState().test).toBeUndefined()
-  store.injectReducer('test', reducer)
-  expect(store.getState().test).toEqual(data)
-  expect(store.getState().test.key).toEqual(1)
+  store.injectReducer('test1', reducer)
+  expect(store.getState().test1).toEqual(data)
+  expect(store.getState().test1.key).toEqual(1)
+  store.injectReducer('test2', reducer)
+  expect(store.getState().test2).toBeUndefined()
+})
+
+test('should add logger middleware in dev mode', () => {
+  const nodeEnv = process.env.NODE_ENV
+  process.env.NODE_ENV = 'development'
+  configureStore(null)
+  expect(logger.default).toBeCalled()
+  process.env.NODE_ENV = nodeEnv
 })
